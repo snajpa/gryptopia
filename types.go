c95cac0f300dfb91de714756e78607dc7bddd021
Pavel Snajdr <snajpa@snajpa.net>
2017-11-13 22:34:01 +0100

    try 5s commit and 15s data refresh

--- types.go
+++ types.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 )
 
-const MainSleep =			15 * time.Second
-const ScannerSleep =		14 * time.Second
+const MainSleep =			5 * time.Second
+const ScannerSleep =		15 * time.Second
 const HTTPDialTimeout = 	30 * time.Second
 const HTTPTLSTimeout = 		60 * time.Second
 const HTTPClientTimeout = 	90 * time.Second
